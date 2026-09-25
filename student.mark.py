15de2379f1af7f4002519ae0a94526d9104a2a78
thachhhhh <tranbaothach2007@gmail.com>
2026-09-25 22:24:33 +0700

    Update student.mark.py

--- student.mark.py
+++ student.mark.py
@@ -23,12 +23,13 @@ def input_students_information():# adding infos into list
 def student_lists():# list all of added info in list
     print("\n----Student List ----")
     for student in students:
-        print(f"ID:{student['id']}, Name:{student['name']},DoB:{student['dob']}")
+        print(f"ID:{student['id']}\nName:{student['name']}\nDoB:{student['dob']}\n-----------------")
 #------------------------------------------------
 def input_number_of_courses():#add total courses
     return int(input("Enter number of courses: "))
 
 def input_course_info():#add courses info
+    print("\n------ Course_Info---------")
     num = input_number_of_courses()
     for i in range(num):
         c_id = input("Course ID: ")
@@ -54,7 +55,7 @@ def input_course_marks():#add marks to courses
     for course in courses:
         print(f"ID:{course['id']},Name:{course['name']}")
     #Select courses
-    course_id = input("\nEnter Course ID")
+    course_id = input("\nEnter Course ID: ").strip()
     #check if course exists
     selected_course = None
     for course in courses:
@@ -62,7 +63,7 @@ def input_course_marks():#add marks to courses
             selected_course = course
             break
     if not selected_course:
-        print(f"Course with ID {course_id}not found.")
+        print(f"Course with ID {course_id} not found.")
         return
 
     #loop through student and input marks
@@ -80,7 +81,8 @@ def input_course_marks():#add marks to courses
                 print("Invalid input. Please enter an actual number")
 #--------------------------------------------
 def show_student_marks():#show student marks
-    course_id = input("\nEnter Course ID to view marks: ")
+    print("\n-------Student Marks-----------")
+    course_id = input("\nEnter Course ID to view marks: ").strip()
     print(f"\n---- Marks for Course ID: {course_id}----")
     found = False
     for student in students:
@@ -95,7 +97,7 @@ input_students_information()
 student_lists()
 
 input_course_info()
-course_lists()
+# course_lists()
 
 input_course_marks()
 show_student_marks()
